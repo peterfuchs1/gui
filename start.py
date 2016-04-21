01bd5c898b60186ee7b7d0783d5d8599986bdc86
peterfuchs1 <peterfuchs_1@yahoo.de>
2016-04-21 14:37:15 +0000

    first commit

---
+++ start.py
@@ -0,0 +1,16 @@
+from PySide import QtCore, QtGui
+from dialog import Ui_Dialog
+import sys
+
+
+class Dialog(QtGui.QDialog):
+    def __init__(self, parent=None):
+        super(Dialog, self).__init__(parent)
+        dialog = Ui_Dialog()
+        dialog.setupUi(self)
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    dialog = Dialog()
+    sys.exit(dialog.exec_())
+
